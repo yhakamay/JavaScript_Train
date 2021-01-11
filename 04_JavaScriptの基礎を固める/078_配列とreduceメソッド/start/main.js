cb09921ac8d3a3862bed073631bc7921f0eb3f29
Yusuke <yhakamaya1031@gmail.com>
2021-01-11 18:32:46 +0900

    modify: just a little re-factoring on some files

--- 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
+++ 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
@@ -1,1 +1,19 @@
-const arry = [1,2,3,4,5];
+const arr = [1, 2, 3, 4, 5];
+
+arr.reduce(function (accu, curr) {
+	console.log(accu, curr);
+	return accu + curr;
+})
+
+const person = {
+	firstName: "Makkie",
+	age: 35,
+	gender: male
+};
+
+thankYouMakkie(firstName, age) => {
+	;
+	if (age != 22)
+		console.log("Thank you " + firstName + "!");
+}
+thankYouMakkie(person.firstName, person.age);
